6d4dd933fded1049785cf901f51164fabcd59314
Michelle Wu <michelle@Michelles-MacBook-Pro.local>
2023-11-13 15:51:08 +1300

    create tag component and map through tags

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -2,17 +2,22 @@ import React from 'react';
 import cn from 'classnames';
 import style from './experience.mod.scss';
 import Text from '../text/text';
+import Tag from '../tag/tag';
 
-const Experience = ({ experiences }) => {
-  console.log(experiences);
+const Experience = ({ title, subtitle, text, tags }) => {
   const classes = cn(style.experience);
 
   return (
     <div className={classes}>
       <div className={style.experience__wrapper}>
-        <div className={style.experience__title}>{experiences.title}</div>
-        <div className={style.experience__subtitle}>{experiences.subtitle}</div>
-        <Text html white text={experiences.text} />
+        <div className={style.experience__title}>{title}</div>
+        <div className={style.experience__subtitle}>{subtitle}</div>
+        <Text html white text={text} />
+        <div className={style.experience__tags}>
+          {tags.map((tag) => (
+            <Tag {...tag} />
+          ))}
+        </div>
       </div>
     </div>
   );
