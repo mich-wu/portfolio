f963e1c840dde6ae5a4ecebd4ae4fc919de018e3
Michelle Wu <michelle@Michelles-MBP.hub>
2023-11-15 15:58:07 +1300

    bring in tab from mui

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -2,15 +2,43 @@ import React, { useState } from 'react';
 import cn from 'classnames';
 import style from './tabs.mod.scss';
 import Codeblock from '../codeblock/codeblock';
+import { Tabs, Tab } from '@mui/material';
 
 const TabContent = ({ components }) => {
   const classes = cn(style.tabs);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div className={classes}>
-      {components.map((component) => (
-        <Codeblock {...component} />
+      <Tabs value={value} onChange={handleChange} centered scrollButtons="auto">
+        {components.map((component, index) => (
+          <Tab label={component.title} key={index} disableRipple />
+        ))}
+      </Tabs>
+      {components.map((component, index) => (
+        <TabPanel value={value} index={index} key={index}>
+          <Codeblock {...component} />
+        </TabPanel>
       ))}
+    </div>
+  );
+};
+
+const TabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      {...other}
+    >
+      {value === index && <div>{children}</div>}
     </div>
   );
 };
